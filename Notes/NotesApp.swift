caf302bef11d3db3af527dd51958b79a6788c6c4
ppham-eagle <127586320+ppham-eagle@users.noreply.github.com>
2023-04-02 12:58:48 -0700

    commit1

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by user236826 on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
